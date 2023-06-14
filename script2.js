f0002adf88f7e105a3416b003367a5db3a61bfad
Julakyan <131914675+Julakyan@users.noreply.github.com>
2023-06-15 00:13:20 +0400

    Add files via upload

--- script2.js
+++ script2.js
@@ -1,100 +1,37 @@
-//1
-let celsius = +prompt("Enter Celsius");
-let fahrenheit = celsius * 1.8 + 32;
-console.log(fahrenheit);
-//----------------------------------------------------------
-
-//2
-let a = 4;
-
-let b = 8;
-
-if (b % a === 0 || a % b === 0) {
-  console.log(1);
-} else {
-  console.log(0);
+let arr = ["a","b","c","d"]
+function checkTextInArr(arr,item){
+    if(arr.includes(item)){
+        return true
+    } else {
+        return false
+    }
 }
 
-// --------------------------------------------------------------
-// 3
-let monthNumber = 5;
-switch (monthNumber) {
-  case 1:
-    console.log("հունվար");
-    break;
-  case 2:
-    console.log("փետրվար");
-    break;
-  case 3:
-    console.log("Մարտ");
-    break;
-  case 4:
-    console.log("Ապրիլ");
-    break;
-  case 5:
-    console.log("Մայիս");
-    break;
-  case 6:
-    console.log("հունիս");
-    break;
-  case 7:
-    console.log("հուլիս");
-    break;
-  case 8:
-    console.log("օգոստոս");
-    break;
-  case 9:
-    console.log("Սեպտեմբեր");
-    break;
-  case 10:
-    console.log("Հոկտեմբեր");
-    break;
-  case 11:
-    console.log("Նոյեմբեր");
-    break;
-  case 12:
-    console.log("Դեկտեմբեր");
-    break;
+console.log(checkTextInArr(arr,"d"))
 
-  default:
-    console.log("sxal");
-    break;
+function addItemInArr (text, method){
+    if(method === 'end'){
+        arr.push (text)
+    } else if(method=== 'start'){
+        arr.unshift (text)
+     }else{
+        alert('method eror')
+     }
+    
+} 
+console.log(arr)
+addItemInArr('test','start')
+console.log(arr)
+
+function changeText (text, s){
+ let textArr = text.split('')
+    let result = ''
+    for (let i = 0; i < textArr.length;i++){
+        result += textArr[i]+ s
+
+    }
+     return result
 }
+console.log(changeText('test', '0'))
 
-// --------------------------------------------------------
-
-// 4
-let a = -6;
-
-let b = -12;
-
-let c = 0;
-
-if (a >= b && a >= c) {
-  console.log(a + " is maximum");
-} else if (c >= b && c >= a) {
-  console.log(c + " is maximum");
-} else if (b >= a && b >= c) {
-  console.log(b + " is maximum");
-}
-// -------------------------------------------------------------
-
-// 5
-let number = +propmpt("Enter Number");
-console.log(Math.abs(number));
-
-// ------------------------------------------
-
-//6
-let start = -12;
-
-let end = 9;
-
-let currentNumber = -12;
-
-if (currentNumber > start && currentNumber < end) {
-  console.log("in bounds");
-} else {
-  console.log("out of bounds");
-}
 
